ba681a27f138b63dc20d17dfb47a2faccec3b972
Cusatelli <einarvandevelde@gmail.com>
2022-02-03 08:54:41 +0100

    chore(Routing): added routing module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module'; // CLI imports 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule // CLI adds AppRoutingModule to the AppModule's 
   ],
   providers: [],
   bootstrap: [AppComponent]
